4fb1d645bdb83aa0c101bed772e4beaa881b8d50
Phuc Duong <phuc.duongdinh@Gmail.com>
2017-09-22 12:40:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080
 const app = express()
     // this assumes that all your app files
     // `public` directory relative to where your server.js is
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(path.join__dirname,'public')))
 
 app.get('*', function(request, response) {
     response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
